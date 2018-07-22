a9aa3a454650cfff8cc7a68314321093915fe7d4
vdoan98 <vdoan98@gmail.com>
2018-07-22 11:45:26 -0500

    Remove unneeded codes

--- k_means.py
+++ k_means.py
@@ -31,11 +31,6 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     for ii, pp in enumerate(pred):
         plt.scatter(features[ii][0], features[ii][1], color = colors[pred[ii]])
 
-    ### if you like, place red stars over points that are POIs (just for funsies)
-    if mark_poi:
-        for ii, pp in enumerate(pred):
-            if poi[ii]:
-                plt.scatter(features[ii][0], features[ii][1], color="r", marker="*")
     plt.xlabel(f1_name)
     plt.ylabel(f2_name)
     plt.savefig(name)
