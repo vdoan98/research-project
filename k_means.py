ddc2bc4fcf4e109afbdd9036057e007933d67aff
vdoan98 <vdoan98@gmail.com>
2018-07-24 11:03:43 -0500

    Clean code

--- k_means.py
+++ k_means.py
@@ -27,26 +27,6 @@ vectorizer = CountVectorizer(min_df=1)
 data = vectorizer.fit_transform([tweet])
 
 
-#Graphing data
-#def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
-#   """ some plotting code designed to help you visualize your clusters """
-
-    ### plot each cluster with a different color--add more colors for
-    ### drawing more than five clusters
-#    colors = ["b", "c", "k", "m", "g"]
-#    for ii, pp in enumerate(pred):
-#        plt.scatter(features[ii][0], features[ii][1], color = colors[pred[ii]])
-
-#    plt.xlabel(f1_name)
-#    plt.ylabel(f2_name)
-#    plt.savefig(name)
-#    plt.show()
-
-
-# for f1, f2 in tweet_features:
-#     plt.scatter( f1, f2 )
-# plt.show()
-
 from sklearn.cluster import KMeans
 import numpy as np
 
